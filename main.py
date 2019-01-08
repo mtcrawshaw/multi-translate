9624566c55bb57839f34ca4f7b9c4e635df694cd
mtcrawshaw <mtcrawshaw@yahoo.com>
2019-01-08 18:33:41 -0500

    Initial test

---
+++ main.py
@@ -0,0 +1,7 @@
+from watson_developer_cloud import LanguageTranslatorV3
+
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+	iam_apikey='OzUwpfx26Fy7pP9xQx33oZADzoy1VUV3moShTfeCi3_b',
+	url='https://gateway.watsonplatform.net/language-translator/api')
+	
